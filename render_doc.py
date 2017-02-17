87cdd59609e721a964e9740b9fab21fdf0a14bc4
Victor Stinner <vstinner@redhat.com>
2017-02-17 22:29:52 +0100

    add git commits

--- render_doc.py
+++ render_doc.py
@@ -9,38 +9,64 @@ def parse_date(text):
 def format_date(date):
     return date
 
-def sort_key(vuln):
-    return parse_date(vuln['disclosure'])
+class Commit:
+    def __init__(self, commit):
+        self.commit = commit
+
+    def url(self):
+        return 'https://github.com/python/cpython/commit/' + self.commit
+
+    @property
+    def short(self):
+        return self.commit[:7]
+
+
+class Vulnerability:
+    def __init__(self, data):
+        self.name = data['name']
+        self.disclosure = parse_date(data['disclosure'])
+        self.description = data['description'].rstrip()
+        self.links = data.get('links')
+        self.fixed_in = []
+        for item in data['fixed-in']:
+            for version, commit in item.items():
+                commit = Commit(commit)
+                self.fixed_in.append((version, commit))
+        self.fixed_in.sort()
+
+
+    @staticmethod
+    def sort_key(vuln):
+        return vuln.disclosure
+
 
 def main(filename):
+    vulnerabilities = []
     with open("vulnerabilities.yml", encoding="utf-8") as fp:
-        vulnerabilities = yaml.load(fp)
+        for data in yaml.load(fp):
+            vuln = Vulnerability(data)
+            vulnerabilities.append(vuln)
+
+    vulnerabilities.sort(key=Vulnerability.sort_key)
 
     headers = ['Bug', 'Disclosure', 'Fixed In', 'Vulnerable', 'Comment']
     table = []
     sections = []
 
-    vulnerabilities.sort(key=sort_key)
 
     for vuln in vulnerabilities:
-        name = vuln['name']
-        disclosure = parse_date(vuln['disclosure'])
-        description = vuln['description']
-
-        fixed_in = []
-        for item in vuln['fixed-in']:
-            for version, commit in item.items():
-                fixed_in.append('{}: {}'.format(version, commit))
+        fixed_in = ['{}: {}'.format(version, commit.short)
+                    for version, commit in vuln.fixed_in]
         # FIXME: one per line, support multilines
         fixed_in = ', '.join(fixed_in)
 
         vulnerable = 'XXX'
         # FIXME: support multilines
-        description = description.replace("\n", " ")
+        description = vuln.description.replace("\n", " ")
 
-        name = "`%s`_" % name
+        name = "`%s`_" % vuln.name
 
-        row = [name, format_date(disclosure), fixed_in, vulnerable, description]
+        row = [name, format_date(vuln.disclosure), fixed_in, vulnerable, description]
         table.append(row)
 
     with open(filename, 'w', encoding='utf-8') as fp:
@@ -55,14 +81,23 @@ def main(filename):
             print(file=fp)
             print(file=fp)
 
-            name = vuln['name']
+            name = vuln.name
 
             print(name, file=fp)
             print("=" * len(name), file=fp)
             print(file=fp)
-            print(vuln['description'].rstrip(), file=fp)
+            print(vuln.description, file=fp)
 
-            links = vuln.get('links')
+            if vuln.fixed_in:
+                print(file=fp)
+                print("Fixed In:", file=fp)
+                for version, commit in vuln.fixed_in:
+                    short = commit.short
+                    url = commit.url()
+                    print("* {}: `commit {} <{}>`_".format(version, short, url),
+                          file=fp)
+
+            links = vuln.links
             if links:
                 print(file=fp)
                 print("Links:", file=fp)
