ed4573c008c8a6c35dc88a63c19207dfa5e9700a
Victor Stinner <victor.stinner@gmail.com>
2017-07-25 12:05:23 +0200

    venv.sh: don't download get-pip.py twice

--- venv.sh
+++ venv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e -x
 python3 -m venv --without-pip venv
-wget https://bootstrap.pypa.io/get-pip.py
+if [ ! -e get-pip.py ]; then
+    wget https://bootstrap.pypa.io/get-pip.py
+fi
 venv/bin/python get-pip.py
 venv/bin/python -m pip install -r requirements.txt
