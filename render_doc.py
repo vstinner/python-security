2722dc5460b0821bd3a5574499e11f65dc6ddd4b
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2019-04-30 12:55:42 +0530

    Add cookiejar and http vulnerabilities

--- render_doc.py
+++ render_doc.py
@@ -16,7 +16,7 @@ import xmlrpc.client
 import yaml
 
 OFFLINE = True
-PYTHON_SRCDIR =  '/home/vstinner/prog/python/master'
+PYTHON_SRCDIR = '/Users/karthikeyansingaravelan/stuff/python/cpython/'
 
 # Last update: 2019-05-13
 MAINTAINED_BRANCHES = ['2.7', '3.5', '3.6', '3.7']
