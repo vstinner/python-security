fbf188c61b3d10d73d63cc6f10085624e7ea247e
Victor Stinner <vstinner@python.org>
2020-05-14 22:18:35 +0200

    Don't get use get-pip.py

--- venv.sh
+++ venv.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -e -x
-python3 -m venv --without-pip venv
-if [ ! -e get-pip.py ]; then
-    wget https://bootstrap.pypa.io/get-pip.py
-fi
-venv/bin/python get-pip.py
+python3 -m venv venv
 venv/bin/python -m pip install -r requirements.txt
